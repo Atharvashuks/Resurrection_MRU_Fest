cd76a57f9fba39ca5501924f139c1810df517f6a
Peeyush Kumar <90157980+Peeyush642@users.noreply.github.com>
2022-07-31 23:37:50 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,9 +3,5 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-// import Sponsers from './Sponsers';
-// import Team from './Team';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<Sponsers />, document.getElementById("root"));
-// ReactDOM.render(<Team />, document.getElementById("root"));
